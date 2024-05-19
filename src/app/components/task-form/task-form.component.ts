ff0c8aec97ea95cf2e59f9a7541e9c9b9e33c16c
Denys Shchablevskyi <dendhg.rgbg@gmail.com>
2024-05-19 21:27:13 +0300

    [11] [TASK] added possibility to add task description and display created/edition date

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -4,6 +4,7 @@ import { TaskStatus, Task } from '../../models';
 
 interface TaskForm {
   name: FormControl<string>;
+  description: FormControl<string>;
   taskStatus: FormControl<TaskStatus>;
 }
 
@@ -19,16 +20,18 @@ export class TaskFormComponent {
   readonly TaskStatus = TaskStatus;
   readonly taskForm = this.formBuilder.group<TaskForm>({
     name: this.formBuilder.control('', [Validators.required]),
+    description: this.formBuilder.control(''),
     taskStatus: this.formBuilder.control(TaskStatus.Backlog),
   })
 
   constructor(private formBuilder: NonNullableFormBuilder) { }
 
   submitForm() {
-    const { name, taskStatus } = this.taskForm.getRawValue();
+    const { name, taskStatus, description } = this.taskForm.getRawValue();
     const createdTask: Task = {
       name,
       taskStatus,
+      description,
       id: String(new Date().getTime()),
       createdDate: new Date().getTime(),
     }
